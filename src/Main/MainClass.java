2b062d11e94918888e352c0326181ee42b593d39
Styn van de Haterd <styn.haterd@rocdeleijgraaf.nl>
2018-06-24 23:42:17 +0200

    #Added version numbering #Added GUI centering

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,9 +1,17 @@
+/*=============================================================================
+ |   	Project:  Pokemon Isle Gen
+ |	  Developer:  Styn van de Haterd
+ |  Description:  A program to generate island like structures that can be imported into AdvancedMap.
+ |     Language:  Java 8       
+ *===========================================================================*/
 package Main;
 
 import Controller.Controller;
 
 public class MainClass {
 
+	public final static double VERSION_NUMBER = 1.0;
+	
 	public static void main(String[] args) {
 		new Controller();
 	}
--- src/View/GUI.java
+++ src/View/GUI.java
@@ -1,31 +1,40 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
 import Controller.Controller;
+import Main.MainClass;
 import Model.Map;
 
 public class GUI extends JFrame {
 
 	private Map map;
-	
+
 	public GUI(Controller controller, Map map) {
 		this.map = map;
-		
+
+		this.setResizable(false);
 		this.updateTitle();
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		
+
 		this.add(new ControlPanel(controller, map), BorderLayout.WEST);
 		this.add(new MapPane(map), BorderLayout.CENTER);
-		
+
 		this.pack();
+
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
 		this.setVisible(true);
 	}
 
 	public void updateTitle() {
-		this.setTitle("PKMN - IsleGen (" + this.map.getWidth() + "x" + this.map.getHeight() + ")");
+		this.setTitle("PKMN - IsleGen Ver " + MainClass.VERSION_NUMBER + " (" + this.map.getWidth() + "x"
+				+ this.map.getHeight() + ")");
 	}
 }
